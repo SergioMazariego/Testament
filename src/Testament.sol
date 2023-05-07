ca30fb57e122e3208411bbd10589bbd92cdd9aff
Your Name <you@example.com>
2023-05-07 16:27:36 -0600

    natspec comment added

--- src/Testament.sol
+++ src/Testament.sol
@@ -6,50 +6,77 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-// Declare the contract.
+/**
+ * @title Testament
+ * @dev A smart contract for managing inheritance payouts.
+ */
 contract Testament is AccessControl {
 
-    // Use SafeMath library to prevent math errors such as overflows.
+    // Use SafeMath library to prevent math errors such as overflows in complex arithmetic.
     using SafeMath for uint256;
     
-    // Declare state variables.
+    // Current inheritance percentage available
+    uint256 availablePercentage;
+
+    // Variable that holds the unix time at when this contract was deployed.
     uint256 deploymentTime;
-    // Stores the time after which the inheritance can be claimed
+    // Stores the time after which the inheritance can be claimed in days
     uint256 inheritanceClaimingAllowedAfter;
 
-    // Declare role identifiers.
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    // Declaration of the beneficiary role
     bytes32 public constant BENEFICIARY_ROLE = keccak256("BENEFICIARY");
 
     // Declare the Beneficiary struct.
     struct Beneficiary {
+        // Name of the beneficiary
         string name;
+        // Wallet of the beneficiary
         address payable wallet;
-        // Define the percentage in the smallest units because Solidity doesn't take decimals.
+        // Define the percentage of the corresponding payment in the smallest units because Solidity doesn't take decimals (two decimals values has been stablished).
         uint256 percentage;
     }
 
-    // Declare events.
+    /**
+     * @dev Event to show a beneficiary's information.
+     * @param name The name of the beneficiary.
+     * @param wallet The wallet address of the beneficiary.
+     * @param percentage The percentage of the inheritance that the beneficiary is entitled to.
+     */
     event ShowBenenficiary(string name, address wallet, uint percentage);
+
+    /**
+     * @dev Event to show the amount of inheritance claimed by a beneficiary.
+     * @param amount The amount of inheritance claimed by the beneficiary.
+    */
     event inheritanceClaimed(uint amount);
 
     // Declare an array to hold all Beneficiaries.
     Beneficiary [] public beneficiaries;
 
-    // Declare the constructor.
+    /**
+     * @dev Grants the admin role to the contract deployer and sets the deployment time and the time after which the inheritance can be claimed.
+     * @param _inheritanceClaimingAllowedAfter The number of days after which the inheritance can be claimed.
+     */
     constructor(uint256 _inheritanceClaimingAllowedAfter) {
         // Grant the ADMIN_ROLE to the contract deployer.
-        _grantRole(ADMIN_ROLE, msg.sender);
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         // Set the deployment time to the current block timestamp.
         deploymentTime = block.timestamp;
         // Set the time after which the inheritance can be claimed.
         inheritanceClaimingAllowedAfter = _inheritanceClaimingAllowedAfter;
     }
 
-    // Declare the function to create a new Beneficiary.
+    /**
+     * @dev Creates a new Beneficiary and adds them to the beneficiaries array.
+     * @param _name The name of the new Beneficiary.
+     * @param _wallet The wallet address of the new Beneficiary.
+     * @param _percentage The percentage of the inheritance that the new Beneficiary will receive.
+     */
     function createBeneficiary(string memory _name, address payable _wallet, uint _percentage) public {
         // Check if the caller has the ADMIN_ROLE.
-        require(hasRole(ADMIN_ROLE, msg.sender), "You are not the admin of this contract");
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "You are not the admin of this contract");
+        //Inheritance is between 1 and 100 percent, with two decimal places.
+        require(percentage >= 100 && percentage <= 10,000, "Percentage must be between 1 and 100 percent");
         // Declare a new Beneficiary instance.
         Beneficiary memory beneficiary;
         beneficiary.name = _name;
@@ -61,10 +88,13 @@ contract Testament is AccessControl {
         _grantRole(BENEFICIARY_ROLE, beneficiary.wallet);
     }
 
-    // Declare the function to delete a Beneficiary.
+    /**
+     * @dev Deletes a Beneficiary from the beneficiaries array.
+     * @param _wallet The wallet address of the Beneficiary to be deleted.
+     */
     function deleteBeneficiary(address _wallet) public {
         // Check if the caller has the ADMIN_ROLE.
-        require(hasRole(ADMIN_ROLE, msg.sender), "You are not the admin of this contract");
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "You are not the admin of this contract");
         // Loop through all Beneficiaries in the array.
         for (uint i = 0; i < beneficiaries.length; i++) {
             // Check if the wallet address of the current Beneficiary matches the input address.
@@ -75,17 +105,26 @@ contract Testament is AccessControl {
         }
     }
 
-    // Declare the function to list all Beneficiaries.
+    /**
+     * @dev Declare the function to list all beneficiaries.
+     * @notice This function can only be called by the admin of this contract.
+     */
     function listBeneficiaries() public {
         // Check if the caller has the ADMIN_ROLE.
-        require(hasRole(ADMIN_ROLE, msg.sender), "You are not the admin of this contract");
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "You are not the admin of this contract");
         // Loop through all Beneficiaries in the array and emit an event for each one.
         for (uint i = 0; i < beneficiaries.length; i++) {
             emit ShowBenenficiary(beneficiaries[i].name, beneficiaries[i].wallet, beneficiaries[i].percentage);
         }
     }
 
-    // Declare the function to claim inheritance if the caller is a beneficiary
+    /**
+     * @dev Declare the function to claim inheritance if the caller is a beneficiary.
+     * @notice This function can only be called by a beneficiary of this contract.
+     * @notice Inheritance can only be claimed after a specified number of days after contract deployment.
+     * @notice The caller's wallet will receive a percentage of the total balance, based on their percentage of the total inheritance.
+     * @notice If the transfer of funds fails, the function will revert.
+     */
     function claimInheritance() public {
         // Check if the caller has the BENEFICIARY_ROLE.
         require(hasRole(BENEFICIARY_ROLE, msg.sender), "You are not a beneficiary");
@@ -109,6 +148,83 @@ contract Testament is AccessControl {
         require(sent, "Failed to send Ether");
         // Emit an event to show the amount claimed by the beneficiary.
         emit inheritanceClaimed(amountBeneficiary);
+    }
+
+    /**
+     * @dev Declare the function to renounce inheritance.
+     * @notice This function can only be called by a beneficiary of this contract.
+     * @notice If there are no beneficiaries left, the function will revert.
+     * @notice The caller's beneficiary role will be revoked, and they will be removed from the list of beneficiaries.
+     */
+    function renounceInheritor() public {
+        require(hasRole(BENEFICIARY_ROLE, msg.sender), "Caller is not a beneficiary of inheritance");
+        // Percentage of inheritance is re-distributed across left beneficiaries
+        
+        // To do: add require if no beneficiary is left
+        // Beneficiary renounce inheritance, beneficiary role is revoked
+        revokeRole(BENEFICIARY_ROLE, msg.sender);
+        // Beneficiary is deleted from the list of beneficiaries
+        delete beneficiaries[getBeneficiaryId()];                   
+    }
+
+
+/**
+ * @dev Returns information about a beneficiary.
+ * @return A tuple containing the name, wallet address, percentage, and ID of the beneficiary.
+ * @notice This function can only be called by the beneficiary whose information is being requested.
+ * @notice If the beneficiary is not found, the function will revert.
+ */
+function getBeneficiaryInfo() public view returns (string memory, address payable, uint256, uint256) {
+    uint id;
+    for (uint i = 0; i < beneficiaries.length; i++) {
+        // Check if the wallet address of the current Beneficiary matches the input address.
+        if (beneficiaries[i].wallet == msg.sender) { 
+            id = i;
+            return (beneficiaries[i].name, beneficiaries[i].wallet, beneficiaries[i].percentage, id);         
+        }
+    }
+    // Revert if the beneficiary is not found
+    revert();
+}
+
+    /**
+     * @dev Retrieves the name of the beneficiary associated with the current account.
+     * @return The name (string) of the beneficiary.
+     */
+    function getBeneficiaryName() public view returns (string memory) {
+        // Call the getBeneficiaryInfo() function to retrieve the tuple, and assign the first element to the name variable.
+        (string memory name, , , ) = getBeneficiaryInfo();
+        return name;
+    }
+
+    /**
+     * @dev Retrieves the wallet address of the beneficiary associated with the current account.
+     * @return The wallet address (address payable) of the beneficiary.
+     */
+    function getBeneficiaryWallet() public view returns (address payable) {
+        // Call the getBeneficiaryInfo() function to retrieve the tuple, and assign the second element to the wallet variable.
+        ( , address payable wallet, , ) = getBeneficiaryInfo();
+        return wallet;
+    }
+
+    /**
+     * @dev Retrieves the percentage of the inheritance that the beneficiary associated with the current account is entitled to.
+     * @return The percentage (uint256) of the inheritance that the beneficiary is entitled to.
+     */
+    function getBeneficiaryPercentage() public view returns (uint256) {
+        // Call the getBeneficiaryInfo() function to retrieve the tuple, and assign the third element to the percentage variable.
+        ( , , uint256 percentage, ) = getBeneficiaryInfo();
+        return percentage;
+    }
+
+    /**
+     * @dev Retrieves the ID of the beneficiary associated with the current account.
+     * @return The ID (uint256) of the beneficiary.
+     */
+    function getBeneficiaryId() public view returns (uint256) {
+        // Call the getBeneficiaryInfo() function to retrieve the tuple, and assign the fourth element to the id variable.
+        ( , , , uint256 id) = getBeneficiaryInfo();
+        return id;
     }
 
     // Declare the function to deposit ETH to this contract
